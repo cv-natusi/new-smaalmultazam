941787fa6a41f649f73daddc3662083a5f0278e6
fatkhan05 <watsittoya469@gmail.com>
2024-10-22 16:19:15 +0700

    change app_env to production

--- app/Custom/CustomUrlGenerator.php
+++ app/Custom/CustomUrlGenerator.php
@@ -20,7 +20,7 @@ class CustomUrlGenerator extends UrlGenerator
             return parent::route($name, $parameters, $absolute);
         } catch (RouteNotFoundException $e) {
             $request = new Request;
-            if (App::environment('local')) {
+            if (App::environment('production')) {
                 // $request->merge([
                 //     "log_payload"=>[
                 //         "file" => $e,
